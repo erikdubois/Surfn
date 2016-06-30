694f41aaff6970f152da48bf311c3addf9f2f74c
erikdubois <erik.dubois@gmail.com>
2016-06-30 07:54:37 +0200

    Commit comment : new icons on Thu Jun 30 07:54:37 CEST 2016

--- git.sh
+++ git.sh
@@ -1,9 +1,4 @@
-#!/bin/bash 
-############################################################################
-# Written to be used on 64 bits computers
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-############################################################################
+#!/bin/bash
 #
 #                                       
 # MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
@@ -25,8 +20,55 @@
 #                                      
 #
 #
-############################################################################
-# Below command will backup everything inside the project folder
+##################################################################################################################
+#
+# Current project : Ultimate-Linux-Mint-18
+#
+# Source 	: 	https://github.com/erikdubois/Ultimate-Linux-Mint-18
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+# 
+# More from Erik Dubois
+#
+# Aurora Conky
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/category/linux/aurora-conky/
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Super Ultra Flat Numix Remix
+# Colourfull and playfull icons
+# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+#
+#
+##################################################################################################################
+# If the option -y has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
 
 #echo "# Super-Ultra-Flat-Numix-Remix" >> README.md
 #git init
@@ -35,15 +77,36 @@
 #git remote add origin https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix.git
 #git push -u origin master
 
+# git config --global user.name x
+# git config --global user.email x
+# sudo git config --system core.editor nano
+# git config --global credential.helper cache
+# git config --global credential.helper 'cache --timeout=3600'
 
 
+# Force git to overwrite local files on pull - no merge
+
+# git fetch all
+# git reset --hard orgin/master
 
 
+gtk-update-icon-cache -f Super-Ultra-Flat-Numix-Remix
+gtk-update-icon-cache -f Super-Ultra-Flat-Numix-Remix-Arch-Blue
+gtk-update-icon-cache -f Super-Ultra-Flat-Numix-Remix-Majestic
+gtk-update-icon-cache -f Super-Ultra-Flat-Numix-Remix-Orange
+
+# Below command will backup everything inside the project folder
 git add --all .
 
-# Committing to the local repository with a message containing the time details
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
 curtime=$(date)
-git commit -m "Automatic Backup @ $curtime"
+git commit -m "Commit comment : $input on $curtime"
 
 # Push the local snapshot to a remote destination
-git push origin master
+git push -u origin master
