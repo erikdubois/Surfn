f9d3a439b0c5a281657e2dddd39fac318a8a2dbd
Erik Dubois <erik.dubois@gmail.com>
2017-01-09 18:38:11 +0100

    Comment :  on 18:38:10 09-01-2017

--- git.sh
+++ git.sh
@@ -1,49 +1,37 @@
-#!/bin/bash 
-############################################################################
+#!/bin/bash
+#
+##################################################################################################################
 # Written to be used on 64 bits computers
 # Author 	: 	Erik Dubois
 # Website 	: 	http://www.erikdubois.be
-############################################################################
+##################################################################################################################
+##################################################################################################################
 #
-#                                       
-# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
-# MMm----::-://////////////oymNMd+`     
-# MMd      /++                -sNMd:    
-# MMNso/`  dMM    `.::-. .-::.` .hMN:   
-# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
-#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
-#     NMm  dMM  -MMm  `MMM   dMM. dMM   
-#     NMm  dMM  -MMm  `MMM   dMM. dMM   
-#     NMm  dMM  .mmd  `mmm   yMM. dMM   
-#     NMm  dMM`  ..`   ...   ydm. dMM   
-#     hMM- +MMd/-------...-:sdds  dMM   
-#     -NMm- :hNMNNNmdddddddddy/`  dMM   
-#      -dMNs-``-::::-------.``    dMM   
-#       `/dMNmy+/:-------------:/yMMM  
-#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
-#             \.MMMMMMMMMMMMMMMMMMM    
-#                                      
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
-#
-############################################################################
+##################################################################################################################
+# Remember : git push origin master --force
+# Remember : git reset --hard <old-commit-id>
+# git push -f <remote-name> <branch-name>
 # Below command will backup everything inside the project folder
-
-#echo "# Surfn" >> README.md
-#git init
-#git add README.md
-#git commit -m "first commit"
-#git remote add origin https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix.git
-#git push -u origin master
-
-
-
-
-
 git add --all .
 
-# Committing to the local repository with a message containing the time details
-curtime=$(date)
-git commit -m "Automatic Backup @ $curtime"
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
 
-# Push the local snapshot to a remote destination
-git push origin master
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+curtime=$(date +"%T %d-%m-%Y")
+git commit -m "Comment : $input on $curtime"
+
+# Push the local files to github
+
+git push -u origin master
+
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
