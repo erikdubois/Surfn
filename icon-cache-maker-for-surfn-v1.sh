3c2bdee0f754c62d88dc7432345c06e543c05f13
Erik Dubois <erik.dubois@gmail.com>
2017-01-24 12:29:26 +0100

    Comment :  on 12:29:26 24-01-2017

--- icon-cache-maker-for-surfn-v1.sh
+++ icon-cache-maker-for-surfn-v1.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
 ##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
 #
-#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
 
