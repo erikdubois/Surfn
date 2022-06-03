eff00265a8dccdf2f213120a3831832c62ed1237
Erik Dubois <erik.dubois@gmail.com>
2022-06-03 22:39:21 +0200

    Comment : new icons on Fri Jun  3 10:39:21 PM CEST 2022

--- setup-git-v5.sh
+++ setup-git-v5.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
-#
+#set -e
 ##################################################################################################################
-# Written to be used on 64 bits computers
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
 ##################################################################################################################
 
 # Problem solving commands
@@ -24,50 +40,21 @@
 # git push --set-upstream origin master
 # git reset --hard orgin/master
 
-
-# installing git if not installed for specific distro's
-
-if ! location="$(type -p "git")" || [ -z "git" ]; then
-
-	echo "#################################################"
-	echo "installing git for this script to work"
-	echo "#################################################"
-
-  	sudo apt install git -y
-	# check if apt-git is installed
-	if which apt-get > /dev/null; then
-
-		sudo apt-get install -y git
-
-	fi
-
-	# check if pacman is installed
-	if which pacman > /dev/null; then
-
-		sudo pacman -S --noconfirm git
-
-	fi
-
-	# check if eopkg is installed
-	if which eopkg > /dev/null; then
-
-		sudo eopkg -y install git
-
-	fi
-
-fi
-
-#setting up git
-#https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
-
-git init
+project=$(basename `pwd`)
+echo "-----------------------------------------------------------------------------"
+echo "this is project https://github.com/erikdubois/"$project
+echo "-----------------------------------------------------------------------------"
+git config --global pull.rebase false
 git config --global user.name "Erik Dubois"
 git config --global user.email "erik.dubois@gmail.com"
 sudo git config --system core.editor nano
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=18000'
+#git config --global credential.helper cache
+#git config --global credential.helper 'cache --timeout=32000'
 git config --global push.default simple
 
+git remote set-url origin git@github.com-edu:erikdubois/$project
+
+echo "Everything set"
 
 echo "################################################################"
 echo "###################    T H E   E N D      ######################"
